9a43dcee1739ec26b09385166276288bc20d0fec
Vansh Choudhary <ch@vnsh.in>
2026-03-29 17:36:39 +0000

    erofs-utils: lib: tar: fix fractional PAX mtime parsing
    The fractional part of PAX mtime values was parsed as a plain integer,
    so "123.5" ended up with 5ns instead of 500000000ns.
    
    Scale the fractional part according to its decimal width before storing
    it as nanoseconds. Also normalize negative fractional timestamps and
    reject malformed mtime values with missing digits or trailing junk.
    
    Fixes: 95d315fd7958 ("erofs-utils: introduce tarerofs")
    Signed-off-by: Vansh Choudhary <ch@vnsh.in>

--- lib/tar.c
+++ lib/tar.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <sys/stat.h>
 #include "erofs/print.h"
 #include "erofs/diskbuf.h"
@@ -524,6 +525,9 @@ int tarerofs_parse_pax_header(struct erofs_iostream *ios,
 				eh->link = strdup(value);
 			} else if (!strncmp(kv, "mtime=",
 					sizeof("mtime=") - 1)) {
+				unsigned int ns = 0;
+				int digits = 0;
+
 				ret = sscanf(value, "%lld %n", &lln, &n);
 				if(ret < 1) {
 					ret = -EIO;
@@ -531,12 +535,31 @@ int tarerofs_parse_pax_header(struct erofs_iostream *ios,
 				}
 				eh->st.st_mtime = lln;
 				if (value[n] == '.') {
-					ret = sscanf(value + n + 1, "%d", &n);
-					if (ret < 1) {
+					while (value[n + 1] >= '0' &&
+					       value[n + 1] <= '9') {
+						if (digits < 9)
+							ns = ns * 10 + value[n + 1] - '0';
+						++digits;
+						++n;
+					}
+					if (!digits || value[n + 1] != '\0') {
 						ret = -EIO;
 						goto out;
 					}
-					ST_MTIM_NSEC_SET(&eh->st, n);
+					while (digits++ < 9)
+						ns *= 10;
+					if (ns && value[0] == '-') {
+						if (eh->st.st_mtime == LLONG_MIN) {
+							ret = -EIO;
+							goto out;
+						}
+						--eh->st.st_mtime;
+						ns = 1000000000 - ns;
+					}
+					ST_MTIM_NSEC_SET(&eh->st, ns);
+				} else if (value[n] != '\0') {
+					ret = -EIO;
+					goto out;
 				} else {
 					ST_MTIM_NSEC_SET(&eh->st, 0);
 				}
