erofs/erofs-utils

commit 73e321a0fb3bbf652773f3b5af0c791e9f511809

erofs-utils: lib: consolidate erofs_rebuild_get_dentry()
 - Avoid `memcmps`, which could cause potential out-of-range read risks;

 - Replace `strlen()` and `memchr()` with `strchr()`.

Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Link: https://lore.kernel.org/r/20250620032616.385805-1-hsiangkao@linux.alibaba.com

lib/rebuild.c
@@ -71,18 +71,27 @@ static struct erofs_dentry *erofs_rebuild_mkdir(struct erofs_inode *dir,
 	return d;
 }
 
+struct erofs_dentry *erofs_d_lookup(struct erofs_inode *dir, const char *name)
+{
+	struct erofs_dentry *d;
+
+	list_for_each_entry(d, &dir->i_subdirs, d_child)
+		if (!strcmp(d->name, name))
+			return d;
+	return NULL;
+}
+
 struct erofs_dentry *erofs_rebuild_get_dentry(struct erofs_inode *pwd,
 		char *path, bool aufs, bool *whout, bool *opq, bool to_head)
 {
 	struct erofs_dentry *d = NULL;
-	unsigned int len = strlen(path);
 	char *s = path;
 
 	*whout = false;
 	*opq = false;
 
-	while (s < path + len) {
-		char *slash = memchr(s, '/', path + len - s);
+	while (1) {
+		char *slash = strchr(s, '/');
 
 		if (slash) {
 			if (s == slash) {
@@ -90,60 +99,54 @@ struct erofs_dentry *erofs_rebuild_get_dentry(struct erofs_inode *pwd,
 				continue;
 			}
 			*slash = '\0';
+		} else if (*s == '\0') {
+			break;
 		}
 
-		if (!memcmp(s, ".", 2)) {
-			/* null */
-		} else if (!memcmp(s, "..", 3)) {
-			pwd = pwd->i_parent;
+		if (__erofs_unlikely(is_dot_dotdot(s))) {
+			if (s[1] == '.') {
+				pwd = pwd->i_parent;
+			}
 		} else {
-			struct erofs_inode *inode = NULL;
-
 			if (aufs && !slash) {
-				if (!memcmp(s, AUFS_WH_DIROPQ, sizeof(AUFS_WH_DIROPQ))) {
+				if (!strcmp(s, AUFS_WH_DIROPQ)) {
 					*opq = true;
 					break;
 				}
-				if (!memcmp(s, AUFS_WH_PFX, sizeof(AUFS_WH_PFX) - 1)) {
+				if (!strcmp(s, AUFS_WH_PFX)) {
 					s += sizeof(AUFS_WH_PFX) - 1;
 					*whout = true;
 				}
 			}
 
-			list_for_each_entry(d, &pwd->i_subdirs, d_child) {
-				if (!strcmp(d->name, s)) {
-					if (d->type != EROFS_FT_DIR && slash)
-						return ERR_PTR(-EIO);
-					inode = d->inode;
-					break;
-				}
-			}
-
-			if (inode) {
-				if (to_head) {
+			d = erofs_d_lookup(pwd, s);
+			if (d) {
+				if (d->type != EROFS_FT_DIR) {
+					if (slash)
+						return ERR_PTR(-ENOTDIR);
+				} else if (to_head) {
 					list_del(&d->d_child);
 					list_add(&d->d_child, &pwd->i_subdirs);
 				}
-				pwd = inode;
-			} else if (!slash) {
+				pwd = d->inode;
+			} else if (slash) {
+				d = erofs_rebuild_mkdir(pwd, s);
+				if (IS_ERR(d))
+					return d;
+			} else {
 				d = erofs_d_alloc(pwd, s);
 				if (IS_ERR(d))
 					return d;
 				d->type = EROFS_FT_UNKNOWN;
 				d->inode = pwd;
-			} else {
-				d = erofs_rebuild_mkdir(pwd, s);
-				if (IS_ERR(d))
-					return d;
-				pwd = d->inode;
 			}
+			pwd = d->inode;
 		}
-		if (slash) {
-			*slash = '/';
-			s = slash + 1;
-		} else {
+
+		if (!slash)
 			break;
-		}
+		*slash = '/';
+		s = slash + 1;
 	}
 	return d;
 }

SToPire/erofs-utils-fork

commit ba9c5375ae1d94bafd84ee3606f622addc28d869

erofs-utils: lib: restore path on erofs_rebuild_get_dentry() errors
erofs_rebuild_get_dentry() temporarily writes '\0' over each '/'
in the caller's path to NUL-terminate segments, but two error
returns skip the restoration and leave the path truncated.

Restore the slash before those returns.

Fixes: 73e321a0fb3b ("erofs-utils: lib: consolidate erofs_rebuild_get_dentry()")
Signed-off-by: Vansh Choudhary <ch@vnsh.in>

lib/rebuild.c
@@ -123,8 +123,10 @@ struct erofs_dentry *erofs_rebuild_get_dentry(struct erofs_inode *pwd,
 			d = erofs_d_lookup(pwd, s);
 			if (d) {
 				if (d->type != EROFS_FT_DIR) {
-					if (slash)
+					if (slash) {
+						*slash = '/';
 						return ERR_PTR(-ENOTDIR);
+					}
 				} else if (to_head) {
 					list_del(&d->d_child);
 					list_add(&d->d_child, &pwd->i_subdirs);
@@ -132,8 +134,10 @@ struct erofs_dentry *erofs_rebuild_get_dentry(struct erofs_inode *pwd,
 				pwd = d->inode;
 			} else if (slash) {
 				d = erofs_rebuild_mkdir(pwd, s);
-				if (IS_ERR(d))
+				if (IS_ERR(d)) {
+					*slash = '/';
 					return d;
+				}
 			} else {
 				d = erofs_d_alloc(pwd, s);
 				if (IS_ERR(d))